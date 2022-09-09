868350fa1dc8ec1f436717f49d4d94e56d5c0a76
FaisalIsmail <faisalkhan7842540@gmail.com>
2022-09-10 00:06:50 +0500

    10-09-2022

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import Header from './MyComponent/Header'
-import Todos from './MyComponent/Todos';
-import Footer from './MyComponent/Footer';
+
 function App() {
-    return (
-      <>
-     <Header/>
-     <Todos/>
-     <Footer/>
-    </>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
